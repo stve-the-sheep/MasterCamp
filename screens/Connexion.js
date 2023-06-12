fcb6aeb8ab9512cf3ebcf14c6842abbf0a955e95
steve_the_sheep <81408972+stve-the-sheep@users.noreply.github.com>
2023-06-12 12:14:34 +0200

    Update Connexion.js

--- screens/Connexion.js
+++ screens/Connexion.js
@@ -1,137 +1,75 @@
 import React, { useState } from "react";
-import {
-  StyleSheet,
-  View,
-  TextInput,
-  TouchableOpacity,
-  Text,
-  Image,
-} from "react-native";
-import { useNavigation } from "@react-navigation/native";
-import * as SQLite from "expo-sqlite";
 
 const Connexion = () => {
-  const navigation = useNavigation();
-  const [ID, setID] = useState("");
+  const [numLicence, setNumLicence] = useState("");
   const [password, setPassword] = useState("");
 
-  const db = SQLite.openDatabase("ma_base_de_donnees.db");
-
-  const seConnecter = (numLicence, password) => {
-    return new Promise((resolve, reject) => {
-      db.transaction((tx) => {
-        tx.executeSql(
-          "SELECT * FROM User WHERE ID = ? AND User_passwords = ?",
-          [ID, password],
-          (tx, results) => {
-            if (results.rows.length > 0) {
-              const user = results.rows.item(0);
-              resolve(user);
-            } else {
-              resolve(false);
-            }
-          },
-          (error) => {
-            reject(error);
-          }
-        );
+  const seConnecter = async (numLicence, password) => {
+    try {
+      const response = await fetch("votre_url_de_requete", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ numLicence, password }),
       });
-    });
+
+      if (response.ok) {
+        const user = await response.json();
+        return user;
+      } else {
+        return false;
+      }
+    } catch (error) {
+      throw new Error("Erreur lors de la requête de connexion");
+    }
   };
 
   const handleConnexion = async () => {
     try {
-      const user = await seConnecter(ID, password);
+      const user = await seConnecter(numLicence, password);
       if (user) {
-        navigation.navigate("Profil", { user });
-        navigation.navigate("Ajouter un evenement", { user });
-        // navigation.navigate("Presence", { user });
-        navigation.navigate("Contact", { user });
-        navigation.navigate("Home", { user });
+        // Utilisateur connecté, effectuer les actions nécessaires
       } else {
         // Aucun utilisateur correspondant trouvé, afficher un message d'erreur ou effectuer d'autres actions
       }
     } catch (error) {
-      // Erreur lors de l'exécution de la requête SQL, gérer l'erreur appropriée
+      // Erreur lors de l'exécution de la requête de connexion, gérer l'erreur appropriée
     }
   };
 
   return (
-    <View style={styles.background}>
-      <Image source={require("../Images/Logo.png")} style={styles.logo} />
-      <Text style={styles.titre}>EasyOrga</Text>
-      <View style={styles.container}>
-        <Text style={styles.texte}>Numéro de licence</Text>
-        <TextInput
-          style={styles.textezone}
-          keyboardType="numeric"
+    <div className="background">
+      <img src={require("../Images/Logo.png")} className="logo" alt="Logo" />
+      <h1 className="titre">EasyOrga</h1>
+      <div className="container">
+        <label className="texte">Numéro de licence</label>
+        <input
+          type="text"
+          className="textezone"
           placeholder="..."
           value={numLicence}
-          onChangeText={setNumLicence}
+          onChange={(e) => setNumLicence(e.target.value)}
         />
 
-        <Text style={styles.texte}>Mot de passe</Text>
-        <TextInput
-          style={styles.textezone}
+        <label className="texte">Mot de passe</label>
+        <input
+          type="password"
+          className="textezone"
           placeholder="..."
-          secureTextEntry
           value={password}
-          onChangeText={setPassword}
+          onChange={(e) => setPassword(e.target.value)}
         />
-        <TouchableOpacity style={styles.button} onPress={handleConnexion}>
-          <Text style={{ color: "white" }}>Se connecter</Text>
-        </TouchableOpacity>
+        <button className="button" onClick={handleConnexion}>
+          Se connecter
+        </button>
 
-        <TouchableOpacity onPress={() => navigation.navigate("Inscription")}>
-          <Text style={{ color: "white" }}>S'inscrire</Text>
-        </TouchableOpacity>
-      </View>
-    </View>
+        <button onClick={() => navigation.navigate("Inscription")}>
+          S'inscrire
+        </button>
+      </div>
+    </div>
   );
 };
-
-const styles = StyleSheet.create({
-  titre: {
-    fontSize: 25,
-    color: "white",
-    fontWeight: "bold",
-    padding: 30,
-  },
-  background: {
-    flex: 1,
-    backgroundColor: "#232c53",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-  container: {
-    width: "80%",
-  },
-  button: {
-    backgroundColor: "#556297",
-    alignItems: "center",
-    width: "100%",
-    paddingHorizontal: 20,
-    paddingVertical: 10,
-    borderRadius: 5,
-    marginVertical: 10,
-  },
-  texte: {
-    color: "white",
-  },
-  textezone: {
-    backgroundColor: "white",
-    borderRadius: 5,
-    width: "100%",
-    paddingHorizontal: 20,
-    paddingVertical: 10,
-    borderRadius: 5,
-    marginVertical: 10,
-  },
-  logo: {
-    alignSelf: "center",
-    width: 200,
-    height: 200,
-  },
-});
 
 export default Connexion;
