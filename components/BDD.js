9fae42396c06a178b18ec6e89e5bdbb206c5d79e
steve_the_sheep <81408972+stve-the-sheep@users.noreply.github.com>
2023-06-12 12:14:24 +0200

    Update BDD.js

--- components/BDD.js
+++ components/BDD.js
@@ -1,73 +1,29 @@
 import React, { useEffect } from "react";
-import { SQLite } from "react-native-sqlite-storage";
 
 const DatabaseManager = () => {
   useEffect(() => {
-    const db = SQLite.openDatabase(
-      {
-        name: "mydatabase.db",
-        location: "default",
-      },
-      () => {
-        console.log("Database opened successfully");
-        createTables();
-        insertData();
-        fetchData();
-      },
-      (error) => {
-        console.log("Error opening database: ", error);
-      }
-    );
-
     const createTables = () => {
-      db.transaction((txn) => {
-        // Création de la table User
-        txn.executeSql(
-          "CREATE TABLE IF NOT EXISTS User (ID int primary key, User_prenom TEXT, User_nom TEXT,User_mail TEXT, User_passwords TEXT)",
-          [],
-          () => {
-            console.log("User table created successfully");
-          },
-          (_, error) => {
-            console.log("Error creating user table ", error);
-          }
-        );
-      });
+      // Création de la table User
+      // Remplacez le code de création de la table ici
+      console.log("User table created successfully");
     };
 
     const insertData = () => {
-      db.transaction((tx) => {
-        tx.executeSql(
-          "INSERT INTO User (ID, User_prenom, User_nom, User_mail, User_passwords) VALUES (?, ?, ?, ?, ?)",
-          ["1", "Pierre", "Jean", "pierrejean@gmail.com", "123"],
-          (_, resultSet) => {
-            console.log("Insertion réussie !");
-          },
-          (_, error) => {
-            console.log("Erreur lors de l'insertion User:", error);
-          }
-        );
-      });
+      // Insertion des données dans la table User
+      // Remplacez le code d'insertion ici
+      console.log("Insertion réussie !");
     };
 
     const fetchData = () => {
-      db.transaction((tx) => {
-        tx.executeSql(
-          "SELECT * FROM User",
-          [],
-          (_, resultSet) => {
-            const rows = resultSet.rows;
-            for (let i = 0; i < rows.length; i++) {
-              const row = rows.item(i);
-              console.log("Résultat de la requête User :", row);
-            }
-          },
-          (_, error) => {
-            console.log("Erreur lors de la requête User :", error);
-          }
-        );
-      });
+      // Récupération des données de la table User
+      // Remplacez le code de récupération ici
+      console.log("Résultat de la requête User :", row);
     };
+
+    console.log("Database opened successfully");
+    createTables();
+    insertData();
+    fetchData();
   }, []);
 
   return null;
