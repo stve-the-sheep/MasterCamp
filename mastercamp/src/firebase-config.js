1a2fde3549285c67a4945d9466516f6d9346c058
steve_the_sheep <81408972+stve-the-sheep@users.noreply.github.com>
2023-06-14 10:41:58 +0200

    clés a decouvert pour tous

--- mastercamp/src/firebase-config.js
+++ mastercamp/src/firebase-config.js
@@ -2,12 +2,12 @@ import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth"
 
 const firebaseConfig = {
-  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
-  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.REACT_APP_FIREBASE_APP_ID
+  apiKey: "AIzaSyB6riQ0ZZAgt8uQrHPcF0cLmcujDIVnPPQ",
+  authDomain: "sport-mc-218.firebaseapp.com",
+  projectId: "sport-mc-218",
+  storageBucket: "sport-mc-218.appspot.com",
+  messagingSenderId: "129951252712",
+  appId: "1:129951252712:web:d6d56471208cc9b435095d"
 };
 
 const app = initializeApp(firebaseConfig);
