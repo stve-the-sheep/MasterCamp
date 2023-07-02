46c2f3d875988409d34e9f8fdf3e2164234ff193
Fennelys <118210391+Fennelys@users.noreply.github.com>
2023-07-02 19:45:53 +0200

    Home Page Pictures
    3 photos
    css de home page
    js de home page

--- mastercamp/src/screens/Home.js
+++ mastercamp/src/screens/Home.js
@@ -1,14 +1,41 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import "./Home.css";
+import footAccImage from "./assets/footAcc.jpeg";
+import tennisAccImage from "./assets/tennisAcc.jpg";
 
 const HomePage = () => {
   return (
     <div className="container">
-      <h2>Bienvenue sur notre site !</h2>
-      <h2>Sign Up ou Sign In</h2>
-      <p>Ceci est la page d'accueil.</p>
+      <div id="introAccueil">
+        <h1>Accueil</h1>
+        <p>Bienvenu sur notre site ! Retrouvez toutes les actualités :</p>
+        <div id="info">
+        <div className="pagefoot">
+          <Link to="/page1" className="page-link"></Link>
+        </div>
+        <div className="pagetennis">
+          <Link to="/page2" className="page-link"></Link>
+        </div>
+      </div>
+      
+      <div className="container">
+      <div id="info">
+        <a href="https://www.lequipe.fr/Football/" className="page-link">
+          <img src={footAccImage} alt="Image de foot" className="image" />
+        </a>
+        <a href="https://www.lequipe.fr/Tennis/" className="page-link">
+          <img src={tennisAccImage} alt="Image de tennis" className="image" />
+        </a>
+      </div>
+    </div>
+      </div>
     </div>
   );
 };
 
 export default HomePage;
+
+/*
+
+*/
